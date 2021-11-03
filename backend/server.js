df00a7adf06f4d92a78a89cfe3e38ea0010b1eca
Dwayne Hovington <dhov92@outlook.com>
2021-11-03 14:03:28 -0400

    debugging

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import http from 'http';
 import { Server } from "socket.io";
+import { createServer } from "http";
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -49,7 +50,7 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5000;
 
-const httpServer = http.Server(app);
+const httpServer = createServer();
 
 const io = new Server(httpServer, { cors: { origin: '*' } });
 const users = [];
