829ed50a48b70da04895a5596987acf835b145f8
Dwayne Hovington <dhov92@outlook.com>
2021-11-03 13:39:08 -0400

    3

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import { Server, Socket }  from 'socket.io';
+import { Server } from "socket.io";
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
